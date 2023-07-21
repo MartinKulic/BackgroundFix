4d9b016ebc3a98b42b6f6c106638f4598707b05c
Martin Kulich <kulich@stud.uniza.sk>
2023-07-21 16:36:56 +0200

    Displays change detected

--- BackgroundFix/Form1.cs
+++ BackgroundFix/Form1.cs
@@ -12,9 +12,30 @@ namespace BackgroundFix
 {
     public partial class Form1 : Form
     {
+        private int displayCount;
         public Form1()
         {
             InitializeComponent();
+            displayCount = Screen.AllScreens.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.AppendText("Screens:");
+            foreach (Screen screen in Screen.AllScreens) {
+                this.richTextBox1.AppendText("\nworkArea: "+screen.WorkingArea + "\nbounds: " + screen.Bounds + "\nbitsPerBixell: " + screen.BitsPerPixel + "\ndeviceName: " +screen.DeviceName);
+                this.richTextBox1.AppendText("\n----------------");
+            }
+            
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (displayCount != Screen.AllScreens.Length) {
+                displayCount = Screen.AllScreens.Length;
+                this.richTextBox1.AppendText("\nWinProc:" + m + "\nAllScreans.len:" + displayCount);
+            }
+            base.WndProc(ref m);
         }
     }
 }
