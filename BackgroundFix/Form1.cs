e83fb2ec3e848227c60516900f6d8b7c84e1a443
Martin Kulich <kulich@stud.uniza.sk>
2023-07-21 19:05:57 +0200

    Change wallpaper acording to number of displays (1 or 2)

--- BackgroundFix/Form1.cs
+++ BackgroundFix/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,9 @@ namespace BackgroundFix
     public partial class Form1 : Form
     {
         private int displayCount;
+       // private const string resources = "./res/";
+        private const string resources = "D:/Junk/VisualStudio/,C#/Repositories/BackgroundFix/BackgroundFix/bin/Debug/res/";
+        private bool toggle = false;
         public Form1()
         {
             InitializeComponent();
@@ -26,16 +30,44 @@ namespace BackgroundFix
                 this.richTextBox1.AppendText("\nworkArea: "+screen.WorkingArea + "\nbounds: " + screen.Bounds + "\nbitsPerBixell: " + screen.BitsPerPixel + "\ndeviceName: " +screen.DeviceName);
                 this.richTextBox1.AppendText("\n----------------");
             }
-            
+
+            if (toggle)
+            {
+                toggle = false;
+                WallpaperChanger.SilentSet(resources + "Dualscreen.jpg");
+            } else
+            {
+                toggle = true;
+                WallpaperChanger.SilentSet(resources + "Singelscreen.png");
+            }
         }
 
         protected override void WndProc(ref Message m)
         {
             if (displayCount != Screen.AllScreens.Length) {
                 displayCount = Screen.AllScreens.Length;
+
+                switch (displayCount)
+                {
+                    case 1:
+                        WallpaperChanger.SilentSet(resources + "Singelscreen.png");
+                        break;
+                    case 2:
+                        WallpaperChanger.SilentSet(resources + "Dualscreen.jpg");
+                        break;
+                    default:
+                        this.richTextBox1.AppendText("\nDefault number of displays not recognised");
+                        break;
+                }
+
                 this.richTextBox1.AppendText("\nWinProc:" + m + "\nAllScreans.len:" + displayCount);
             }
             base.WndProc(ref m);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WallpaperChanger.RestoreState();
         }
     }
 }
