e23abfb872f044bc99481a803329f2bde8a109ff
Martin Kulich <kulich@stud.uniza.sk>
2024-07-14 21:48:45 +0200

    Added events Correctly recognize and log change in display count

--- BackBroundFix_WorkerService/WatcherService.cs
+++ BackBroundFix_WorkerService/WatcherService.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Management;
 using System.Runtime.InteropServices;
-
-
+using System.Windows.Forms;
 
 
 
@@ -11,11 +10,39 @@ namespace BackBroundFix_WorkerService
 {
     public class WatcherService
     {
+        public event EventHandler<int> DisplayCountChanged;
+        
+        private ManagementEventWatcher watcher;
+       
+        public int displayCount { get; set; }
+
+        public WatcherService()
+        {
+            displayCount = getMonitorCount();
+
+            try
+            {
+                string queryString = "SELECT * FROM __InstanceOperationEvent WITHIN 5 WHERE TargetInstance ISA 'Win32_PnPEntity' AND TargetInstance.Service = 'monitor'";
+                WqlEventQuery query = new WqlEventQuery(queryString);
+
+                watcher = new ManagementEventWatcher(query);
+                watcher.EventArrived += new EventArrivedEventHandler(OnDisplayChange);
+                watcher.Start();
+
+                DisplayCountChanged?.Invoke(this, displayCount);
+            }
+            catch (Exception ex)
+            {
+                //ignore
+            }
+        }
+
+
 
         public string getMessage()
         {
-           
-            return $"Time: {DateTimeOffset.Now}{Environment.NewLine}Number of monitors: {getMonitorCount()}";
+           displayCount = getMonitorCount();
+            return $"Time: {DateTimeOffset.Now}{Environment.NewLine}Number of monitors: {displayCount}";
         }
 
         private int getMonitorCount()
@@ -24,11 +51,7 @@ namespace BackBroundFix_WorkerService
 
             try
             {
-                ManagementScope scope = new("\\\\.\\ROOT\\cimv2");
-                scope.Connect();
-                ObjectQuery query = new("SELECT * FROM Win32_PnPEntity WHERE Service = 'monitor'");
-
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"\\.\ROOT\cimv2", "SELECT * FROM Win32_PnPEntity WHERE Service = 'monitor'");
                 ManagementObjectCollection result = searcher.Get();
 
                 monitorCount = result.Count;
@@ -36,10 +59,31 @@ namespace BackBroundFix_WorkerService
             catch (Exception ex)
             {
                 //ignore
+                return monitorCount;
             }
 
             return monitorCount;
         }
 
+        private void OnDisplayChange(object sender, EventArrivedEventArgs e)
+        {
+            int newDisplaycount = getMonitorCount();
+            if (newDisplaycount != displayCount)
+            {
+                displayCount = newDisplaycount;
+                DisplayCountChanged?.Invoke(this, displayCount);
+            }
+        }
+        private void OnDisplayCountChanged(int newDisplayCount)
+        {
+            DisplayCountChanged?.Invoke(this, newDisplayCount);
+        }
+
+        public void Dispose()
+        {
+
+        }
     }
+
+
 }
--- BackBroundFix_WorkerService/Worker.cs
+++ BackBroundFix_WorkerService/Worker.cs
@@ -2,21 +2,43 @@ using System.Windows.Forms;
 
 namespace BackBroundFix_WorkerService
 {
-    public class Worker(WatcherService watcherService, ILogger<Worker> logger) : BackgroundService
+    public class Worker() : BackgroundService
     {
-        
+        private readonly WatcherService watcherService;
+        private readonly ILogger<Worker> logger;
+
+        public Worker(WatcherService awatcherService, ILogger<Worker> alogger) : this()
+        {
+            this.watcherService = awatcherService;
+            this.logger = alogger;
+
+            watcherService.DisplayCountChanged += WhenDisplayCountChange;
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                if (logger.IsEnabled(LogLevel.Information))
-                {
-                    string message = watcherService.getMessage();
-                    logger.LogWarning(message);
-                }
-                await Task.Delay(10000, stoppingToken);
-            }
+
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            //    if (logger.IsEnabled(LogLevel.Information))
+            //    {
+            //        string message = watcherService.getMessage();
+            //        logger.LogWarning(message);
+            //    }
+            //    await Task.Delay(10000, stoppingToken);
+            //}
+        }
+
+        void WhenDisplayCountChange(object sender, int newDisplayCount)
+        {
+            logger.LogInformation($"Change in number of display: {newDisplayCount}");
+        }
+
+        public override void Dispose()
+        {
+            watcherService.DisplayCountChanged -= WhenDisplayCountChange;
+            watcherService.Dispose();
+            base.Dispose();
         }
     }
 }
