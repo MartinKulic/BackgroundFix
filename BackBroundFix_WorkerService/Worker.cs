0fa9db2d3085b4eecec03f2d4b4bafc5bc20355e
Martin Kulich <kulich@stud.uniza.sk>
2024-07-15 15:27:00 +0200

    Added toast/application notification when number of displayes changes

--- BackBroundFix_WorkerService/Worker.cs
+++ BackBroundFix_WorkerService/Worker.cs
@@ -1,18 +1,21 @@
+using Microsoft.Toolkit.Uwp.Notifications;
 using System.Windows.Forms;
 
 namespace BackBroundFix_WorkerService
 {
-    public class Worker() : BackgroundService
+    public class Worker : BackgroundService
     {
         private readonly WatcherService watcherService;
         private readonly ILogger<Worker> logger;
 
-        public Worker(WatcherService awatcherService, ILogger<Worker> alogger) : this()
+        public Worker(WatcherService awatcherService, ILogger<Worker> alogger)
         {
             this.watcherService = awatcherService;
             this.logger = alogger;
 
             watcherService.DisplayCountChanged += WhenDisplayCountChange;
+
+            WhenDisplayCountChange(this, watcherService.displayCount);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -32,6 +35,12 @@ namespace BackBroundFix_WorkerService
         void WhenDisplayCountChange(object sender, int newDisplayCount)
         {
             logger.LogInformation($"Change in number of display: {newDisplayCount}");
+
+            var builder = new ToastContentBuilder()
+                .AddArgument("conversationId", 9813)
+                .AddText("Change in connected displayes")
+                .AddText($"New number of displayes: {watcherService.displayCount}");
+            builder.Show(toast => { toast.ExpirationTime = DateTime.Now.AddSeconds(60); });
         }
 
         public override void Dispose()
